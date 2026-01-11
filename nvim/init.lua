625ae6943fb11214fc35e2e8a4926aa64ff84cb3
hatrickek <halimbayka@outlook.com>
2026-01-11 17:16:09 +0300

    up nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -522,8 +522,7 @@ require('lazy').setup({
             completion = {
               callSnippet = 'Replace',
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
+            diagnostics = { disable = { 'missing-fields', 'undefined-global', 'lowercase-global' } },
           },
         },
       })
@@ -575,20 +574,7 @@ require('lazy').setup({
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { lua = false, c = false, cpp = false }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
+      format_on_save = false,
       formatters_by_ft = {
         lua = { 'stylua' },
         nix = { 'nixfmt' },
